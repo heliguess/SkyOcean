f161945a3c6ea5b64a93e76d696a0f95e91c17d4
heliguess <retreat743@gmail.com>
2026-02-23 17:13:21 +0100

    feat: midas bid breakdown

---
+++ src/main/kotlin/me/owdding/skyocean/features/item/lore/MidasPaidBreakdownModifier.kt
@@ -0,0 +1,43 @@
+package me.owdding.skyocean.features.item.lore
+
+import me.owdding.skyocean.config.features.lorecleanup.LoreModifierConfig
+import me.owdding.skyocean.features.item.modifier.AbstractItemModifier
+import me.owdding.skyocean.features.item.modifier.ItemModifier
+import me.owdding.skyocean.utils.Utils.unaryPlus
+import net.minecraft.network.chat.Component
+import net.minecraft.world.item.ItemStack
+import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
+import tech.thatgravyboat.skyblockapi.api.datatype.getData
+import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedString
+import tech.thatgravyboat.skyblockapi.utils.text.TextColor
+import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
+import tech.thatgravyboat.skyblockapi.utils.text.TextBuilder.append
+
+@ItemModifier
+object MidasPaidBreakdownModifier : AbstractItemModifier() {
+    override val displayName: Component = +"skyocean.config.lore_modifiers.midas_bid_breakdown"
+    override val isEnabled: Boolean get() = LoreModifierConfig.midasBidBreakdown
+
+
+    override fun appliesTo(itemStack: ItemStack) = itemStack.getData(DataTypes.MIDAS_WEAPON_ADDED_COINS) != null
+
+    override fun modifyTooltip(item: ItemStack, list: MutableList<Component>, previousResult: Result?) = withMerger(list) {
+        val originalBid = item.getData(DataTypes.MIDAS_WEAPON_BID) ?: return@withMerger null
+        val addedCoins = item.getData(DataTypes.MIDAS_WEAPON_ADDED_COINS) ?: return@withMerger null
+
+        addAfterNext({it.stripped.contains("Price paid: ")}){
+            add{
+                append("Original Bid: ") { color = TextColor.DARK_GRAY }
+                append(originalBid.toFormattedString()) { color = TextColor.GOLD }
+            }
+            add{
+                append("Added Coins: ") { color = TextColor.DARK_GRAY }
+                append(addedCoins.toFormattedString()) { color = TextColor.GOLD }
+            }
+
+        }
+
+        Result.modified
+    }
+}
