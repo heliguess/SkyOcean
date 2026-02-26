044f4c5780c298c890577938d2133cac49333b22
Mona <git@mona.cat>
2026-02-26 08:29:39 +0100

    fix: loading custom items too early

--- src/main/kotlin/me/owdding/skyocean/features/item/custom/CustomItems.kt
+++ src/main/kotlin/me/owdding/skyocean/features/item/custom/CustomItems.kt
@@ -9,6 +9,7 @@ import me.owdding.skyocean.SkyOcean
 import me.owdding.skyocean.accessors.customize.ItemStackAccessor
 import me.owdding.skyocean.config.features.misc.MiscConfig
 import me.owdding.skyocean.features.item.custom.data.*
+import me.owdding.skyocean.utils.LateInitModule
 import me.owdding.skyocean.utils.codecs.CodecHelpers
 import me.owdding.skyocean.utils.storage.DataStorage
 import net.minecraft.world.item.ItemStack
@@ -24,7 +25,7 @@ import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 import kotlin.uuid.toJavaUuid
 
-@Module
+@LateInitModule
 object CustomItems : MeowddingLogger by SkyOcean.featureLogger() {
 
     private val map: MutableMap<ItemKey, CustomItemData> = mutableMapOf()
